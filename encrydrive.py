e82814a43f9db951ca028f2cdf20b821514649e5
Sarah Hale <sarahlauhale@gmail.com>
2013-11-16 18:33:45 -0800

    Made encrydrive.py
    At this point it's the same as quickstart.
    I just wanted to make one for the final/whole thing.

---
+++ encrydrive.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+
+import httplib2
+import pprint
+
+from apiclient.discovery import build
+from apiclient.http import MediaFileUpload
+from oauth2client.client import OAuth2WebServerFlow
+from apiclient import errors
+
+def download_file(service, drive_file):
+  """Download a file's content.
+
+  Args:
+    service: Drive API service instance.
+    drive_file: Drive File instance.
+
+  Returns:
+    File's content if successful, None otherwise.
+  """
+  download_url = drive_file.get('downloadUrl')
+  if download_url:
+    resp, content = service._http.request(download_url)
+    if resp.status == 200:
+      print 'Status: %s' % resp
+      return content
+    else:
+      print 'An error occurred: %s' % resp
+      return None
+  else:
+    # The file doesn't have any content stored on Drive.
+    return None
+
+def retrieve_all_files(service, search_str):
+  """Retrieve a list of File resources.
+
+  Args:
+    service: Drive API service instance.
+  Returns:
+    List of File resources.
+  """
+  result = []
+  page_token = None
+  while True:
+    try:
+      param = {}
+      if page_token:
+        param['pageToken'] = page_token
+      #if search_str:
+        #param['q'] = search_str
+      files = service.files().list(**param).execute()
+
+      result.extend(files['items'])
+      page_token = files.get('nextPageToken')
+      if not page_token:
+        break
+    except errors.HttpError, error:
+      print 'An error occurred: %s' % error
+      break
+  return result
+
+## START SETUP
+
+# Copy your credentials from the console
+CLIENT_ID = '525402384233-lg16n5981raug5jc2ou8rghs2je0s16d.apps.googleusercontent.com'
+CLIENT_SECRET = 'zaITwVEagm-bYu4sSd-Z3-T4'
+
+# Check https://developers.google.com/drive/scopes for all available scopes
+OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
+
+# Redirect URI for installed apps
+REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
+
+# Path to the file to upload
+FILENAME = 'document.txt'
+
+# Run through the OAuth flow and retrieve credentials
+flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
+authorize_url = flow.step1_get_authorize_url()
+print 'Go to the following link in your browser: ' + authorize_url
+code = raw_input('Enter verification code: ').strip()
+credentials = flow.step2_exchange(code)
+
+# Create an httplib2.Http object and authorize it with our credentials
+http = httplib2.Http()
+http = credentials.authorize(http)
+
+drive_service = build('drive', 'v2', http=http)
+
+# Insert a file
+media_body = MediaFileUpload(FILENAME, mimetype='text/plain', resumable=True)
+body = {
+  'title': 'My document',
+  'description': 'A test document',
+  'mimeType': 'text/plain'
+}
+
+file = drive_service.files().get(fileId='0B2rUt6t49XG_SmRUbzQ2MWtzckE').execute()
+
+#file = drive_service.files().insert(body=body, media_body=media_body).execute()
+
+#for item in retrieve_all_files(drive_service,'Chapter'):
+  #pprint.pprint(item)
+
+#print download_file(drive_service,"")
+#pprint.pprint(file)
+
+pprint.pprint(download_file(drive_service,file))
